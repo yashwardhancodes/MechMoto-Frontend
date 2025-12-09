4b786d1fe5a9772faa976000977db058a75fbcfc
manish-badgujar <badgujarmanish999@gmail.com>
2025-12-09 23:53:50 +0530

    check: carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -7,58 +7,54 @@ import Image from 'next/image';
 
 const Carousel = () => {
   return (
-    <div className="mb-3 md:mb-4 lg:mb-6">
-      <Swiper
-        modules={[Pagination, Autoplay]}
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 2000, disableOnInteraction: false }}
-        spaceBetween={20}
-        slidesPerView={1}
-        loop={true}
-        className="overflow-hidden"
-      >
-         
-        
-         
-        <SwiperSlide>
-          <Image
-            src="/assets/1.png"
-            alt="Slide 4"
-            width={1200}
-            height={500}
-            className="w-full h-auto"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src="/assets/2.png"
-            alt="Slide 4"
-            width={1200}
-            height={500}
-            className="w-full h-auto"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src="/assets/3.png"
-            alt="Slide 4"
-            width={1200}
-            height={500}
-            className="w-full h-auto"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src="/assets/4.png"
-            alt="Slide 4"
-            width={1200}
-            height={500}
-            className="w-full h-auto"
-          />
-        </SwiperSlide>
-     
-      </Swiper>
-    </div>
+		<div className="mb-3 md:mb-4 lg:mb-6">
+			<Swiper
+				modules={[Pagination, Autoplay]}
+				pagination={{ clickable: true }}
+				autoplay={{ delay: 2000, disableOnInteraction: false }}
+				spaceBetween={20}
+				slidesPerView={1}
+				loop={true}
+				className="overflow-hidden"
+			>
+				<SwiperSlide>
+					<Image
+						src="/assets/mobile1.png"
+						alt="Slide 4"
+						width={1200}
+						height={500}
+						className="w-full h-auto"
+					/>
+				</SwiperSlide>
+				<SwiperSlide>
+					<Image
+						src="/assets/2.png"
+						alt="Slide 4"
+						width={1200}
+						height={500}
+						className="w-full h-auto"
+					/>
+				</SwiperSlide>
+				<SwiperSlide>
+					<Image
+						src="/assets/3.png"
+						alt="Slide 4"
+						width={1200}
+						height={500}
+						className="w-full h-auto"
+					/>
+				</SwiperSlide>
+				<SwiperSlide>
+					<Image
+						src="/assets/4.png"
+						alt="Slide 4"
+						width={1200}
+						height={500}
+						className="w-full h-auto"
+					/>
+				</SwiperSlide>
+			</Swiper>
+		</div>
   );
 };
 
