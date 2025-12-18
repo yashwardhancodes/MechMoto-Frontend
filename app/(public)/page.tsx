dfb3c7291d9f35f47e5f389f9e1106421c69a51e
yashwardhancodes <143013221+yashwardhancodes@users.noreply.github.com>
2025-12-18 09:08:05 +0530

    verifying cicd 3

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -64,7 +64,7 @@ export default function UsersPage() {
             }`}
             onClick={() => setSelectedTab("repair")}
           >
-            Repair my carff
+            Repair my carf
           </button>
         </div>
       </div>
